a01babe1c798f4b474674e019b70ffd0be25a2af
jpcanto <jplopessk8@gmail.com>
2020-07-11 16:17:44 -0300

    [ UPDATE ] - Login form validation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 
@@ -9,6 +9,7 @@ import { IonicModule } from '@ionic/angular';
   declarations: [],
   exports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     IonicModule,
   ]
