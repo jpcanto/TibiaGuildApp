6db3ef37283c7d3049a8466daa456bbb01103392
jpcanto <jplopessk8@gmail.com>
2020-07-12 12:25:54 -0300

    [ UPDATE ] - Inheriting Firestore abstract class

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Firestore } from '../core/classes/firestore.class';
+import { Task } from '../components/tasks/models/task.model';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TasksService {
+export class TasksService extends Firestore<Task> {
 
-  constructor() { }
+  constructor(db: AngularFirestore) {
+    super(db);
+  }
 }
