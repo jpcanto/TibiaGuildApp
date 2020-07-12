c0bd7547563e41aab6f03e9e56129277b6ce4604
jpcanto <jplopessk8@gmail.com>
2020-07-12 12:42:52 -0300

    [ UPDATE ] - Dynamically get firebase collection tasks

--- src/app/views/tasks/tasks-list/tasks-list.page.ts
+++ src/app/views/tasks/tasks-list/tasks-list.page.ts
@@ -1,26 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/components/tasks/models/task.model';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.page.html',
   styleUrls: ['./tasks-list.page.scss'],
 })
-export class TasksListPage implements OnInit {
+export class TasksListPage {
 
   // $ is an observable
 
   tasks$: Observable<Task[]>;
 
-  constructor() { }
+  constructor(private tasksService: TasksService) { }
 
-  ngOnInit(): void {
-    // of returns a new observable
-    this.tasks$ = of([
-      {id: 'teste', title: 'GT', done: false},
-      {id: 'teste2', title: 'Inquisition', done: false}
-    ]);
+  ionViewDidEnter(): void {
+    this.tasks$ = this.tasksService.getAll();
   }
 
 }
