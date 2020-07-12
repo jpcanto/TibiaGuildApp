acddc1a98e691bc51172a2b10ed786687df54a63
jpcanto <jplopessk8@gmail.com>
2020-07-12 15:08:59 -0300

    [ CREATE ] - Create new task algorithm

--- src/app/views/tasks/create-task/create-task.page.ts
+++ src/app/views/tasks/create-task/create-task.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TasksService } from 'src/app/services/tasks.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-task',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTaskPage implements OnInit {
 
-  constructor() { }
+  taskForm: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private navCtrl: NavController,
+    private tasksService: TasksService
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.taskForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(2)]],
+      done: [false]
+    })
+  }
+
+  async onSubmit(): Promise<void> {
+    try {
+      const task = await this.tasksService.create(this.taskForm.value);
+      this.navCtrl.navigateBack('/tasks');
+    } catch (error) {
+      console.log(error)
+    }
   }
 
 }
